36ff00df9e1bec6300e9a4e3499e47f780ad162b
Noah Lively <noahlively@gmail.com>
2024-02-03 21:25:57 -0600

    messing with movement

--- main.go
+++ main.go
@@ -14,8 +14,12 @@ const SCREEN_WIDTH = 800
 const SCREEN_HEIGHT = 600
 const FRAME_RATE = 60 // frames per second
 
-const STAR_RADIUS = 10.0
+const STAR_RADIUS = 4.0
 const UNIVERSE_DEPTH = 2000.0
+
+const VANISHING_X = SCREEN_WIDTH / 2
+const VANISHING_Y = SCREEN_HEIGHT / 2
+const VANISHING_Z = UNIVERSE_DEPTH
 
 type ThreeDPoint struct {
 	x float64
@@ -72,7 +76,7 @@ func (s *Scene) GeneratePoints() {
 	// generate a thousand stars
 	s.stars = make([]ThreeDPoint, 0)
 	for i := 0; i < 1000; i++ {
-		depth := UNIVERSE_DEPTH // rand.Float64() * UNIVERSE_DEPTH
+		depth := rand.Float64() * UNIVERSE_DEPTH
 		star := ThreeDPoint{rand.Float64() * SCREEN_WIDTH, rand.Float64() * SCREEN_HEIGHT, depth}
 		s.stars = append(s.stars, star)
 	}
@@ -91,7 +95,7 @@ func (s *Scene) Update() error {
 	for i := 0; i < len(s.stars); i++ {
 		star := &s.stars[i]
 		star.z -= 1
-		if star.z < 0 {
+		if star.z < 0-UNIVERSE_DEPTH {
 			star.z = UNIVERSE_DEPTH
 		}
 	}
@@ -104,21 +108,19 @@ func (s *Scene) Draw(screen *ebiten.Image) {
 		ebitenutil.DrawLine(screen, line.start.x, line.start.y, line.end.x, line.end.y, line.color)
 	}
 
-	for i, star := range s.stars {
+	for _, star := range s.stars {
 		star_color := color.RGBA{255, 255, 255, 128}
 
-		star_x := star.x * (UNIVERSE_DEPTH / star.z)
-		star_y := star.y * (UNIVERSE_DEPTH / star.z)
+		star_x := star.x + ((VANISHING_X-star.x)/VANISHING_Z)*star.z
+		star_y := star.y + ((VANISHING_Y-star.y)/VANISHING_Z)*star.z
 
 		scale_factor := (UNIVERSE_DEPTH / (UNIVERSE_DEPTH + star.z))
 
 		// adjust star width and height based on distance
 		star_radius := STAR_RADIUS * scale_factor
 
-		if i == 20 {
-			fmt.Printf("Star %d - depth: %f, orig coords: %f, %f coords: %f, %f, radius: %f\n", i, star.z, star.x, star.y, star_x, star_y, star_radius)
-			star_color = color.RGBA{255, 0, 0, 255}
-		}
+		// fmt.Printf("Star %d - depth: %f, orig coords: %f, %f coords: %f, %f, radius: %f\n", i, star.z, star.x, star.y, star_x, star_y, star_radius)
+		// star_color = color.RGBA{255, 0, 0, 255}
 
 		ebitenutil.DrawCircle(screen, star_x, star_y, star_radius, star_color)
 	}
